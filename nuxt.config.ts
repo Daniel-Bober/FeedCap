a3ccc715256f8cc401dcc56c55cb35fa321ecfcf
Daniel <99998739+Daniel-Bober@users.noreply.github.com>
2022-10-23 14:51:03 +0200

    init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({
 
+export default defineNuxtConfig({
+    ssr: false,
+    css: ["~/assets/style/main.scss"],
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData:
+                        '@import "~/assets/style/variables.scss"; ' +
+                        '@import "~/assets/style/mixins.scss";',
+                },
+            },
+        },
+    },
 })
