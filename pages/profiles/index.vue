5c279f120952be298f80f4c478b91ece89ae2a57
Daniel <99998739+Daniel-Bober@users.noreply.github.com>
2022-11-01 21:15:11 +0100

    feat: profiles page

---
+++ pages/profiles/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="profiles-page">
+
+    <span class="header">Choose profile</span>
+
+    <div class="selection-profiles-area">
+      <ProfilesSelection
+          :profiles="profiles"
+      ></ProfilesSelection>
+    </div>
+
+
+    <NuxtLink class="add-button" to="/profiles/new-profile">New profile</NuxtLink>
+  </div>
+</template>
+
+<script setup lang='ts'>
+const profiles = ['burgir', 'Big Burger Bar', 'tekst', 'tekst2', 'coś', 'jeszcze coś innego', 'ostatnie']
+
+</script>
+
+<style lang='scss' scoped>
+.profiles-page {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex-grow: 1;
+
+  .header {
+    font-size: $font-36px;
+    user-select: none;
+  }
+
+  .selection-profiles-area {
+    height: 360px;
+    display: flex;
+    align-items: center;
+  }
+
+  .add-button {
+    font-size: $font-18px;
+    color: $pink;
+    border: 1px solid $gray;
+    border-radius: 20px;
+    padding: 8px 30px;
+    transition: $time-hover-anim;
+    user-select: none;
+  }
+
+  .add-button:hover {
+    transform: scale(1.05);
+  }
+}
+</style>
