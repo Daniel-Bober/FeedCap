25dfbe1ac838abc48d6a8f1db3faaa8e368cc8b3
Daniel <99998739+Daniel-Bober@users.noreply.github.com>
2022-11-11 21:17:39 +0100

    feat: init liked page

---
+++ pages/liked/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="liked-messages-list-page">
+
+    <ScrollList>
+      <list-comp-message :liked-mode="false"></list-comp-message>
+      <list-comp-message :liked-mode="false"></list-comp-message>
+      <list-comp-message :liked-mode="false"></list-comp-message>
+      <list-comp-message :liked-mode="false"></list-comp-message>
+    </ScrollList>
+
+  </div>
+</template>
+
+
+<script setup lang='ts'>
+import ScrollList from "~/components/ListComp/ScrollList.vue";
+
+</script>
+
+
+<style lang='scss' scoped>
+.liked-messages-list-page {
+  height: calc(100vh - 140px);
+}
+</style>
