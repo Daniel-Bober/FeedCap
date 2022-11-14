d9128bdcc05b104546508c5ac560386226c68f7b
Daniel <99998739+Daniel-Bober@users.noreply.github.com>
2022-11-14 23:17:51 +0100

    feat: store

---
+++ stores/user.ts
@@ -0,0 +1,10 @@
+
+export const useUserGlobalState = defineStore('userState', {
+    state: () => {
+        return {
+            isLogged: false
+        };
+    },
+    getters: {},
+    actions: {},
+});
