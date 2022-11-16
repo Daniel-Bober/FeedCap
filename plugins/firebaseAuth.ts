9e47e9e2e80930c787158e6001bf4434e406e69b
Daniel <99998739+Daniel-Bober@users.noreply.github.com>
2022-11-16 21:31:38 +0100

    feat: init firebase auth

---
+++ plugins/firebaseAuth.ts
@@ -0,0 +1,22 @@
+import {initializeApp} from "firebase/app";
+import {getAuth} from "firebase/auth";
+
+
+export default defineNuxtPlugin(nuxtApp => {
+    const config = useRuntimeConfig()
+
+    const firebaseConfig = {
+        apiKey: config.public.apiBase,
+    };
+
+    const app = initializeApp(firebaseConfig);
+
+
+    initUser();
+
+
+    const auth = getAuth();
+
+    nuxtApp.vueApp.provide('auth', auth)
+    nuxtApp.provide('auth', auth)
+})
