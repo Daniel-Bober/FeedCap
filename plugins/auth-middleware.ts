f6154c8b631e9ac621d9b47f30429a968de20d80
Daniel <99998739+Daniel-Bober@users.noreply.github.com>
2022-11-19 23:00:42 +0100

    refactor: code refactoring

--- plugins/auth-middleware.ts
+++ plugins/auth-middleware.ts
@@ -1,9 +1,7 @@
-import {getAuth} from "firebase/auth";
-
 export default defineNuxtPlugin(() => {
     addRouteMiddleware('auth', () => {
         const isLogged = JSON.parse(localStorage.getItem('isLogged'))
-        if(!isLogged) {
+        if (!isLogged) {
             return navigateTo('/login')
         }
     })
