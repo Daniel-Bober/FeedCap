7f585c255e05b72c2ed2c7815d6b610eb94da0a1
Daniel <99998739+Daniel-Bober@users.noreply.github.com>
2022-11-19 22:51:06 +0100

    feat: user global state

--- stores/user.ts
+++ stores/user.ts
@@ -1,10 +1,23 @@
+import {defineStore} from "pinia";
+import {ref} from "#imports";
 
 export const useUserGlobalState = defineStore('userState', {
     state: () => {
         return {
-            isLogged: false
+            isLogged: ref(JSON.parse(localStorage.getItem('isLogged'))),
         };
     },
     getters: {},
-    actions: {},
+    actions: {
+
+        setStatusToLogged() {
+            this.isLogged = true;
+            localStorage.setItem('isLogged', JSON.stringify(true))
+        },
+
+        setStatusToLoggedOut() {
+            this.isLogged = false;
+            localStorage.setItem('isLogged', JSON.stringify(false))
+        }
+    },
 });
