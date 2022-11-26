9df83aafc1d03c7dfe4f08e128328159d02afd70
Daniel <99998739+Daniel-Bober@users.noreply.github.com>
2022-11-26 21:05:26 +0100

    feat: add profile button functionality

--- components/Profiles/Button.vue
+++ components/Profiles/Button.vue
@@ -1,17 +1,25 @@
 <template>
-  <div class="profile-button">{{props.name}}</div>
+  <div>
+    <button class="profile-button" @click="selectProfile">{{props.name}}</button>
+  </div>
 </template>
 
 <script setup lang='ts'>
+import {useMainStore} from "~/stores/mainStore";
+
 const props = defineProps({
   name: String
 })
+const mainStore = useMainStore()
+
+const selectProfile = () => {
+  mainStore.setProfile(props.name)
+  navigateTo('/messages')
+}
 </script>
 
 <style lang='scss' scoped>
 .profile-button {
-  display: grid;
-  place-items: center;
   height: 60px;
   min-width: 130px;
   padding: 0 60px;
@@ -19,12 +27,12 @@ const props = defineProps({
   border: 1px solid $gray;
   border-radius: 30px;
   font-size: $font-24px;
+  color: $offWhite;
   transition: $time-hover-anim;
-  cursor: pointer;
-  user-select: none;
 }
 
 .profile-button:hover {
+  border: 1px solid $lightGray;
   transform: scale(1.03);
 }
 </style>
