b106e20c112a1d1ef4d65d0f5b133840c7109c61
Daniel <99998739+Daniel-Bober@users.noreply.github.com>
2022-11-28 23:39:50 +0100

    feat: add functions: sendMessage, getCustomersMessages, moveMessagesToLiked

--- composables/useFirestore.ts
+++ composables/useFirestore.ts
@@ -1,7 +1,8 @@
-import {collection, getDocs, query, doc, setDoc, updateDoc, where, getDoc} from "firebase/firestore";
+import {collection, getDocs, query, doc, setDoc, updateDoc, where, getDoc, deleteDoc} from "firebase/firestore";
 
 import {getCurrentUser} from "~/composables/useFirebaseAuth";
 import ShortUniqueId from "short-unique-id";
+import {useMainStore} from "~/stores/mainStore";
 
 export const initUser = async (email, username) => {
     const {$firestore} = useNuxtApp();
@@ -22,17 +23,6 @@ export const createProfile = async (profileName) => {
     const {$firestore} = useNuxtApp();
     const { $auth } = useNuxtApp();
     await setDoc(doc($firestore, $auth.currentUser.email, profileName), {});
-};
-
-export const sendMessage = async (email, profileName, customerEmail, customerName, message, date, dateNow) => {
-    const {$firestore} = useNuxtApp();
-
-    await setDoc(doc($firestore, email, profileName, 'messages', dateNow), {
-        customerEmail: customerEmail,
-        customerName: customerName,
-        message: message,
-        date: date
-    });
 };
 
 
@@ -60,6 +50,66 @@ export const getProfiles = async  () => {
     const userInfoIndex = profiles.indexOf('userInfo')
     profiles.splice(userInfoIndex, 1)
     return profiles
+}
+
+export const sendMessage = async (email, profileName, customerEmail, customerName, message, date, dateNow) => {
+    const {$firestore} = useNuxtApp();
+
+    await setDoc(doc($firestore, email, profileName, 'messages', dateNow), {
+        customerEmail: customerEmail,
+        customerName: customerName,
+        message: message,
+        date: date
+    });
+};
+
+export const getCustomersMessages = async (messageType) => {
+    const {$firestore} = useNuxtApp();
+    const mainStore = useMainStore();
+    const user:any = await getCurrentUser();
+    let docName = null;
+
+    switch (messageType) {
+        case 1:
+            docName = 'messages'
+            break;
+
+        case 2:
+            docName = 'likedMessages'
+            break;
+    }
+
+
+    const q = query(collection($firestore, user.email, mainStore.selectedProfile, docName));
+    const querySnapshot = await getDocs(q);
+    const messagesData = [[], []]
+
+
+    querySnapshot.forEach((doc) => {
+        messagesData[0].push(doc.data());
+        messagesData[1].push(doc.id);
+    });
+
+
+    return messagesData
+};
+
+
+export const moveMessageToLiked = async (message, messageID) => {
+    const {$firestore} = useNuxtApp();
+    const mainStore = useMainStore();
+    const user:any = await getCurrentUser();
+
+    await deleteDoc(doc($firestore, user.email, mainStore.selectedProfile, 'messages', messageID));
+
+    const dateNow = Date.now().toString()
+    await setDoc(doc($firestore, user.email, mainStore.selectedProfile, 'likedMessages', dateNow), {
+        customerEmail: message.customerEmail,
+        customerName: message.customerName,
+        message: message.message,
+        date: message.date
+    });
+
 }
 
 
