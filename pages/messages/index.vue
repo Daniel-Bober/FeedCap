2e9bb05671fdad323993f0f5fa3284adaee16074
Daniel <99998739+Daniel-Bober@users.noreply.github.com>
2023-01-09 18:49:33 +0100

    feat: add empty data gate, cleaning

--- pages/messages/index.vue
+++ pages/messages/index.vue
@@ -1,31 +1,39 @@
 <template>
   <div class="messages-page">
-    <div class="message-body">
 
-      <div class="top-icons-group">
-        <NuxtLink class="list-mode-icon" to="/messages/list">
-          <svg width="29" height="17" viewBox="0 0 29 17" xmlns="http://www.w3.org/2000/svg">
-            <path
-                d="M16.1993 15.2657C16.1993 14.6778 15.7222 14.2007 15.1344 14.2007H1.6448C1.05694 14.2007 0.579834 14.6778 0.579834 15.2657C0.579834 15.8535 1.05694 16.3306 1.6448 16.3306H15.1344C15.7222 16.3306 16.1993 15.8535 16.1993 15.2657ZM28.9789 8.1659C28.9789 7.57804 28.5018 7.10093 27.914 7.10093H1.6448C1.05694 7.10093 0.579834 7.57804 0.579834 8.1659C0.579834 8.75376 1.05694 9.23087 1.6448 9.23087H27.914C28.5018 9.23087 28.9789 8.75376 28.9789 8.1659ZM23.2991 1.06613C23.2991 0.478265 22.822 0.00115967 22.2341 0.00115967H1.6448C1.05694 0.00115967 0.579834 0.478265 0.579834 1.06613C0.579834 1.65399 1.05694 2.13109 1.6448 2.13109H22.2341C22.822 2.13109 23.2991 1.65399 23.2991 1.06613Z"/>
-          </svg>
-        </NuxtLink>
+    <div v-if="messages.length !== 0">
+      <div class="message-body" >
 
-        <span class="messages-count">
+        <div class="top-icons-group">
+          <NuxtLink class="list-mode-icon" to="/messages/list">
+            <svg width="29" height="17" viewBox="0 0 29 17" xmlns="http://www.w3.org/2000/svg">
+              <path
+                  d="M16.1993 15.2657C16.1993 14.6778 15.7222 14.2007 15.1344 14.2007H1.6448C1.05694 14.2007 0.579834 14.6778 0.579834 15.2657C0.579834 15.8535 1.05694 16.3306 1.6448 16.3306H15.1344C15.7222 16.3306 16.1993 15.8535 16.1993 15.2657ZM28.9789 8.1659C28.9789 7.57804 28.5018 7.10093 27.914 7.10093H1.6448C1.05694 7.10093 0.579834 7.57804 0.579834 8.1659C0.579834 8.75376 1.05694 9.23087 1.6448 9.23087H27.914C28.5018 9.23087 28.9789 8.75376 28.9789 8.1659ZM23.2991 1.06613C23.2991 0.478265 22.822 0.00115967 22.2341 0.00115967H1.6448C1.05694 0.00115967 0.579834 0.478265 0.579834 1.06613C0.579834 1.65399 1.05694 2.13109 1.6448 2.13109H22.2341C22.822 2.13109 23.2991 1.65399 23.2991 1.06613Z"/>
+            </svg>
+          </NuxtLink>
+
+          <span class="messages-count">
           <img src="~/assets/images/icons/message_gray.svg" alt="message icon">
           <span>{{ messages.length }}</span>
         </span>
+        </div>
+
+
+        <span class="customer-name">{{ messages[0].customerName }}</span>
+
+        <span class="customer-message" role="textbox">{{ messages[0].message }}</span>
+
+        <span class="date">{{ messages[0].date }}</span>
       </div>
 
-      <span class="customer-name">{{ messagesData[0][0].customerName }}</span>
-
-      <span class="customer-message" role="textbox">{{ messagesData[0][0].message }}</span>
-
-      <span class="date">{{ messagesData[0][0].date }}</span>
+      <div class="buttons-group">
+        <button class="delete-button"><img src="@/assets/images/icons/delete.svg" alt="delete icon"></button>
+        <button class="like-button" @click="moveToLiked"><img src="@/assets/images/icons/heart_pink.svg" alt="heart icon"></button>
+      </div>
     </div>
 
-    <div class="buttons-group">
-      <button class="delete-button"><img src="~/assets/images/icons/delete.svg" alt="delete icon"></button>
-      <button class="like-button" @click="moveToLiked"><img src="~/assets/images/icons/heart_pink.svg" alt="heart icon"></button>
+    <div v-else>
+      <span class="emptyTxt">Empty</span>
     </div>
   </div>
 </template>
@@ -35,7 +43,7 @@ const messagesData = ref(await getCustomersMessages(1));// 1-'messages', 2-'like
 const messages = ref(messagesData.value[0])
 const messagesID = ref(messagesData.value[1])
 
-const moveToLiked = async() => {
+const moveToLiked = async () => {
   await moveMessageToLiked(messages.value[0], messagesID.value[0])
   messages.value.splice(0, 1)
   messagesID.value.splice(0, 1)
@@ -128,7 +136,7 @@ definePageMeta({
 
   .buttons-group {
     width: 260px;
-    margin-top: 110px;
+    margin: 110px auto 0 auto;
     display: flex;
     justify-content: space-between;
 
@@ -150,6 +158,11 @@ definePageMeta({
       transform: scale(1);
       transition: $time-click-anim;
     }
+  }
+
+  .emptyTxt {
+    font-size: $font-36px;
+    color: $gray;
   }
 }
 </style>
