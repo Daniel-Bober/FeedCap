8f46ca0afd15a81be408cf620ea7747e17ba1b3d
Daniel <99998739+Daniel-Bober@users.noreply.github.com>
2023-01-13 21:06:54 +0100

    feat: add functionality to give gifts

--- pages/liked/index.vue
+++ pages/liked/index.vue
@@ -8,21 +8,122 @@
           :customer-name="message.customerName"
           :customer-message="message.message"
           :date="message.date"
+          :message-id="likedMessagesID[index]"
           :liked-mode="false"
+          @buttonClicked="openPopUpWindow"
       ></ListCompMessage>
-
     </ScrollList>
+
+    <PopUpWindow
+    :is-window-open="isWindowOpen"
+    @close-window="closePopUpWindow"
+    @give-reward="giveReward"
+    ></PopUpWindow>
 
   </div>
 </template>
 
 
 <script setup lang='ts'>
-import ScrollList from "~/components/ListComp/ScrollList.vue";
+import ScrollList from "@/components/ListComp/ScrollList.vue";
+import PopUpWindow from "~/components/popUpWindow.vue";
+import ShortUniqueId from "short-unique-id";
+import {useMainStore} from "~/stores/mainStore";
+import firestoreProfileColNames from "~/enums/firestoreProfileColNames";
 
-const messagesData = ref(await getCustomersMessages(2));// 1-'messages', 2-'likedMessages',
-const likedMessages = ref(messagesData.value[0])
-const likedMessagesID = ref(messagesData.value[1])
+const mainStore = useMainStore();
+
+const messagesData = ref(await getCustomersMessages(firestoreProfileColNames.LikedMessages));
+const likedMessages = ref(messagesData.value[0]);
+const likedMessagesID = ref(messagesData.value[1]);
+
+
+const profileName = ref("profileName");
+const giftCode = ref("rewardCode");
+const securityCode = ref("securityCode");
+const gift = ref("reward");
+const  customerEmail = ref("daniel13bober@interia.pl");
+const {$sgMail} = useNuxtApp();
+let clickedMessageID = null;
+
+function sendEmail() {
+  const msg = {
+    from: customerEmail.value,
+    template_id: "d-9713cff6c4f6496e8ac64ced4ea9900c",
+    personalizations: [{
+      to: {email: "daniel.bober01@gmail.com"},
+      dynamic_template_data: {
+        profileName: profileName.value,
+        code: giftCode.value,
+        reward: gift.value,
+        securityCode: securityCode.value
+      }
+    }]
+  }
+
+  $sgMail.send(msg)
+      .then(() => {
+        closePopUpWindow()
+        console.log("email sent")
+      })
+      .catch((error) => {
+        console.log("error")
+        console.error(error)
+      })
+}
+
+
+const isWindowOpen = ref(false);
+const createUID = new ShortUniqueId({length: 5});
+const createSecurityCode = new ShortUniqueId({
+  dictionary: ["1", "2", "3", "4", "5", "6", "7", "8", "9",],
+  length: 4})
+
+function openPopUpWindow(messageID) {
+  isWindowOpen.value = true;
+  clickedMessageID = messageID
+}
+
+function closePopUpWindow() {
+  isWindowOpen.value = false;
+}
+
+async function giveReward(rewardValue) {
+  const gCode = createUID();
+  const sCode = createSecurityCode();
+  profileName.value = mainStore.selectedProfile;
+  giftCode.value = gCode;
+  gift.value = rewardValue;
+  securityCode.value = sCode;
+
+  const clickedMessageIndex = likedMessagesID.value.findIndex(el => el === clickedMessageID);
+
+  const giftMessage =  createGiftMessage(clickedMessageIndex);
+
+
+  await moveMessageToNewCol(firestoreProfileColNames.Gifts, giftMessage)
+  likedMessages.value.splice(clickedMessageIndex, 1)
+  likedMessagesID.value.splice(clickedMessageIndex, 1)
+  await deleteDocFromCollection(firestoreProfileColNames.LikedMessages , clickedMessageID);
+
+
+  // sendEmail()
+  //skip sending email
+  closePopUpWindow()
+  //TODO^^^Delete later
+}
+
+function createGiftMessage(clickedMessageIndex) {
+  const giftMessage = likedMessages.value[clickedMessageIndex];
+  giftMessage.giftCode = giftCode.value;
+  giftMessage.gift = gift.value;
+  giftMessage.securityCode = securityCode.value;
+  giftMessage.isGiftActive = true;
+
+  return giftMessage;
+}
+
+
 
 definePageMeta({
   middleware: 'auth'
